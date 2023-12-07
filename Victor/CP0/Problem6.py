505b16249ad496b0a86ed080aaf6f365eb21e29f
sirpoetvic <sirpoetvic@gmail.com>
2023-12-07 13:59:36 -0800

    P6 on CP0 complete

--- Victor/CP0/Problem6.py
+++ Victor/CP0/Problem6.py
@@ -1,3 +1,9 @@
 '''Write a function called count_votes that takes a list of numbers 
    indicating votes for candidates 0, 1, or 2 and returns a new list 
    of length 3 showing how many counts each candidate got.'''
+   
+def count_votes( votes ):
+   list = [0,0,0]
+   for i in range(len(votes)):
+      list[votes[i]] += 1
+   return list
