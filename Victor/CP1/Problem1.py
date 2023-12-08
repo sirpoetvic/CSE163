2febd11ec7d64afb99e985b0167090b45d3ed993
sirpoetvic <sirpoetvic@gmail.com>
2023-12-08 01:14:27 -0800

    P1 complete, P2 needs to be tested (CP1)

--- Victor/CP1/Problem1.py
+++ Victor/CP1/Problem1.py
@@ -1,1 +1,16 @@
 # fun_numbers
+
+'''Write a function called fun_numbers that takes a start number
+and stop number and returns a list of all “fun” numbers between
+start (inclusive) and stop (exclusive). A number is “fun” if it
+is divisible by 2 or divisible by 5. The result should have the
+numbers arranged from smallest to largest.'''
+
+def fun_numbers( start, stop ):
+    list = []
+    for i in range(start, stop):
+        if(i % 2 == 0 or i % 5 == 0):
+            list.append(i)
+    return list
+
+print(fun_numbers(2, 16))
--- Victor/CP1/Problem2.py
+++ Victor/CP1/Problem2.py
@@ -1,1 +1,15 @@
 # count_long_lines
+
+'''Write a method called filter_long_lines that takes a file name
+and a minimum number of words and prints out all of the lines
+in the file with at least that many words (tokens separated by spaces).'''
+
+def filter_long_lines(filename, minnum):
+    with open(filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            tokens = line.split()
+            if(tokens >= minnum):
+                print(line)
+
+        
