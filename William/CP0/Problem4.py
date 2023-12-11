5f8a9928e1756cbe2e831f4b70ca115a118b3c8d
William Dinh <billdinh52@gmail.com>
2023-12-11 14:00:04 -0800

    finished cp0 problem 4

--- William/CP0/Problem4.py
+++ William/CP0/Problem4.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 """
-Write a function named switch_pairs that accepts a string as a parameter and returns that string with each pair of neighboring letters reversed. 
+Write a function named switch_pairs that accepts a string as a parameter and returns that string with each pair of 
+neighboring letters reversed. 
 
 If the string has an odd number of letters, the last letter should not be modified. 
 
-For example, the call switch_pairs("example") should return "xemalpe" and the call switch_pairs("hello there") should return "ehll ohtree".
+For example, the call switch_pairs("example") should return "xemalpe" and the call switch_pairs("hello there") 
+should return "ehll ohtree".
 
 This function returns a string and does not print anything.
 """
 
 def switch_pairs(pair):
-    testing 123
-    print "oh that dont work"
+    str_holder = list(pair)
+    for i in range(0, len(pair)-1, 2):
+        str_holder[i], str_holder[i+1] = str_holder[i+1], str_holder[i]
+    return ''.join(str_holder)
 
+# using to test if the output is actually correct 
 def main():
-    switch_pairs("example")
+    print(switch_pairs("example")) # returns - "xemalpe"
+    print(switch_pairs("hello there")) # returns - "ehll ohtree"
     
 if __name__ == "__main__":
     main()
