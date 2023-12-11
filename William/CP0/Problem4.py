9351fbf7c16bcef322be51209a1ceb86a38de3cb
William Dinh <billdinh52@gmail.com>
2023-12-11 13:24:49 -0800

    ian figures out how replit works

---
+++ William/CP0/Problem4.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Write a function named switch_pairs that accepts a string as a parameter and returns that string with each pair of neighboring letters reversed. 
+
+If the string has an odd number of letters, the last letter should not be modified. 
+
+For example, the call switch_pairs("example") should return "xemalpe" and the call switch_pairs("hello there") should return "ehll ohtree".
+
+This function returns a string and does not print anything.
+"""
+
+def switch_pairs(pair):
+    testing 123
+    print "oh that dont work"
+
+def main():
+    switch_pairs("example")
+    
+if __name__ == "__main__":
+    main()
