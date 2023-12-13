fe16f9c02b492862742b795d8bcec9a315faed92
sirpoetvic <sirpoetvic@gmail.com>
2023-12-13 13:02:04 -0800

    quick changes to HW0 and P4, CP0

--- Victor/HW0/main.py
+++ Victor/HW0/main.py
@@ -1,17 +1,30 @@
+"""
+Don't forget to put your name and information here!
+Name: Victor Wong
+"""
+
+'''Returns linear interpolation of a and b,
+of c
+'''
+
+
 def funky_sum(a, b, mix):
-    if(mix <= 0):
+    if mix <= 0:
         return a
-    elif(mix >= 1):
+    elif mix >= 1:
         return b
     else:
-        return 1-mix * a + mix * b
+        return (1 - mix) * b + mix * a
+
+
+'''Returns the sum of all numbers from 0 (inclusive) to n (inclusive)'''
+
 
 def total(n):
-    if(n < 0):
+    if n < 0:
         return None
-    t = 0
-    for i in range(n):
-        t += i
-    return t
-
-def 
+    else:
+        result = 0
+    for i in range(n + 1):
+        result += i
+    return result
