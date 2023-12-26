6db47baa4485ea161feb8f329fda54a3abfa5b71
sirpoetvic <sirpoetvic@gmail.com>
2023-12-26 12:12:38 -0800

    added earthquakes.csv test file

--- Victor/CP1/Problem8.py
+++ Victor/CP1/Problem8.py
@@ -2,7 +2,10 @@
 
 # https://cse163.github.io/book/module-2-data-structures-and-files/lesson-6-csv-data/practice-largest-earthquake.html
 
+import cse163_utils
+
 
 def largest_earthquake(data):
     if len(data) == 0:
         return None
+    return csv.
