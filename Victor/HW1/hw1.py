5cb58416f4fe8884e57e0097b1018b640409031d
sirpoetvic <sirpoetvic@gmail.com>
2023-12-28 21:29:24 -0800

    Completed majority of HW1, added zip folder for CP submissions

---
+++ Victor/HW1/hw1.py
@@ -0,0 +1,99 @@
+"""
+<name>
+<period>
+<file description>
+"""
+# do not import math
+
+
+from itertools import count
+
+
+def total(n):
+    """
+    Returns the sum of the numbers from 0 to n (inclusive).
+    If n is negative, returns None.
+    """
+    if n < 0:
+        return None
+    else:
+        result = 0
+        for i in range(n + 1):
+            result += i
+        return result
+
+
+def count_divisible_digits(n, m):
+    if m == 0 or n == 0:
+        return 0
+    n = abs(n)
+    if (n % 10) % m == 0:
+        return count_divisible_digits(n // 10, m) + 1
+    else:
+        return count_divisible_digits(n // 10, m)
+
+
+def is_relatively_prime(n, m):
+    """Determines if n and m have a common factor
+
+    Args:
+        n (int): number 1
+        m (int): number 2
+
+    Returns:
+        boolean: if n and m do not have a common factor
+    """
+    for i in range(2, int(max(n, m) / 2)):
+        if n % i == 0 and m % i == 0:
+            return False
+    return True
+
+
+def travel(directions, x, y):
+    for i in directions.lower():
+        if i == "n":
+            y += 1
+        elif i == "s":
+            y -= 1
+        elif i == "e":
+            x += 1
+        elif i == "w":
+            x -= 1
+    return (x, y)
+
+
+def reformat_date(date, current_date_format, target_date_format):
+    return_date = ""
+    date_letters = current_date_format.split("/")
+    target_date_letters = target_date_format.split("/")
+    date_numbers = date.split("/")
+
+    for i in range(len(target_date_letters)):
+        return_date += date_numbers[date_letters.index(target_date_letters[i])] + (
+            ("/" if i != len(target_date_letters) - 1 else "")
+        )
+    return return_date
+
+
+def longest_word(file_name):
+    long_line, long_word, long_word_length = 0, "", 0
+    with open(file_name) as f:
+        lines = f.readlines()
+        if len(lines) == 0:
+            return None
+        for line in enumerate(lines):
+            tokens = line[1].split(" ")
+            for token in tokens:
+                if len(token) > long_word_length:
+                    long_line = line[0] + 1
+                    long_word_length = len(token)
+                    long_word = token
+    return f"{long_line}: {long_word}"
+
+
+def get_average_in_range(values, low, high):
+    pass
+
+
+def mode_digit(n):
+    pass
---
+++ Victor/HW1/hw1_test.py
@@ -0,0 +1,76 @@
+"""
+<name>
+<period>
+<file description>
+"""
+import hw1
+
+from cse163_utils import assert_equals
+
+
+def test_total():
+    """
+    Tests the total method
+    """
+    # The regular case
+    assert_equals(15, hw1.total(5))
+    # Seems likely we could mess up 0 or 1
+    assert_equals(1, hw1.total(1))
+    assert_equals(0, hw1.total(0))
+
+    # Test the None case
+    assert_equals(None, hw1.total(-1))
+
+
+def test_count_divisible_digits():
+    assert_equals(4, hw1.count_divisible_digits(650899, 3))
+    assert_equals(1, hw1.count_divisible_digits(-204, 5))
+    assert_equals(0, hw1.count_divisible_digits(24, 5))
+    assert_equals(0, hw1.count_divisible_digits(1, 0))
+    assert_equals(4, hw1.count_divisible_digits(100002, 5))
+    assert_equals(5, hw1.count_divisible_digits(200000543534543543534, 7))
+
+
+def test_is_relatively_prime():
+    assert_equals(True, hw1.is_relatively_prime(12, 13))
+    assert_equals(False, hw1.is_relatively_prime(12, 14))
+    assert_equals(True, hw1.is_relatively_prime(5, 9))
+    assert_equals(True, hw1.is_relatively_prime(8, 9))
+    assert_equals(True, hw1.is_relatively_prime(8, 1))
+    assert_equals(False, hw1.is_relatively_prime(2, 6))
+    assert_equals(True, hw1.is_relatively_prime(1, 5))
+
+
+def test_travel():
+    assert_equals((-1, 4), hw1.travel("NW!ewnW", 1, 2))
+    assert_equals((0, 0), hw1.travel("eeeewwwwnnnnssss", 0, 0))
+    assert_equals((1, -4), hw1.travel("jfkld;ajkfl;", 1, -4))
+
+
+def test_reformat_date():
+    assert_equals("31/12/1998", hw1.reformat_date("12/31/1998", "M/D/Y", "D/M/Y"))
+    assert_equals("4/2000", hw1.reformat_date("0/2000/4", "D/Y/M", "M/Y"))
+    assert_equals("2", hw1.reformat_date("3/2", "M/D", "D"))
+
+
+def test_longest_word():
+    assert_equals(
+        "3: Merrily,",
+        hw1.longest_word(
+            "C:\\Users\\victo\\OneDrive\\Desktop\\CSE163\\Victor\\HW1\\song.txt"
+        ),
+    )
+
+
+def main():
+    # test_total()
+    test_count_divisible_digits()
+    test_is_relatively_prime()
+    test_travel()
+    test_reformat_date()
+    test_longest_word()
+    # Call your test functions here!
+
+
+if __name__ == "__main__":
+    main()
