53ddac706dbd78d954e842700f8d6eca542eec00
sirpoetvic <sirpoetvic@gmail.com>
2024-03-12 21:39:53 -0700

    manual and pandas complete

--- Victor/HW2/hw2_pandas.py
+++ Victor/HW2/hw2_pandas.py
@@ -3,18 +3,26 @@ Name: Victor Wong
 CSE163
 """
 
-import pandas as pd
-
-DATA = "Victor\\HW2\\pokemon_test.csv"
-data = pd.read_csv(DATA)
-
 
 def species_count(df):
     return len(df["name"].unique())
 
 
 def max_level(df):
-    
+    return (df["name"][df["level"].idxmax()], df["level"].max())
 
 
-print(species_count(data))
+def filter_range(df, lower: int, upper: int):
+    return list(df[(df["level"] >= lower) & (df["level"] < upper)]["name"])
+
+
+def mean_attack_for_type(df, pokemon_type):
+    return df[df["type"] == pokemon_type]["atk"].mean()
+
+
+def count_types(df):
+    return dict(df.groupby("type").size())
+
+
+def mean_attack_per_type(df):
+    return dict(df.groupby("type")["atk"].mean())
