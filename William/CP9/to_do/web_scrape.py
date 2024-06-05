41398d8153f3f7fb16a091491b9860b3655661b4
William Dinh <billdinh52@gmail.com>
2024-06-04 17:15:37 -0700

    finisehd web scrape

--- William/CP9/to_do/web_scrape.py
+++ William/CP9/to_do/web_scrape.py
@@ -7,7 +7,7 @@ def get_selector_table():
     '''
     Reads a webpage and extracts out the contents of a table.
     https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Selectors#reference_table_of_selectors
-    
+
     Returns a dataframe with the content:
     Selector            Example        Description
     .class	            .intro	       Selects all elements with class="intro"
@@ -19,20 +19,35 @@ def get_selector_table():
     url = 'https://www.w3schools.com/cssref/css_selectors.asp'
     page = requests.get(url)
     # create a beautiful soup object that can parse the HTML content
-    
+    soup = BeautifulSoup(page.content, 'html.parser')
     # get the table markup from soup object
-    
+    table = soup.find('table')
     # parse the 3 columns by getting the <td> data that is a child of <tr>
-    
+    trs = soup.find_all('tr')
+
     # create a list for each column, getting the text content as needed
+    selector, example, ex_desc = [], [], []
+
+    for t in trs:
+        trd = t.find_all('td')
+        # add if statement bc of the out of bounds error, since the first
+        # column in the table is html table headers i think, so it is angry
+        if len(trd) == 3:
+            selector.append(trd[0].get_text())
+            example.append(trd[1].get_text())
+            ex_desc.append(trd[2].get_text())
 
     # Create and return a dataframe using the lists created above
-    
+    lists = pd.DataFrame({"Selector": selector,
+                         "Examples": example,
+                          "Example Description": ex_desc})
 
-    
+    return lists
+
+
 def main():
     print(get_selector_table())
 
-    
+
 if __name__ == '__main__':
     main()
